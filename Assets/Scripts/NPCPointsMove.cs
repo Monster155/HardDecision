93f9cf2a16f8dce77c47aa5223ef8a135f3c4522
Nikita <shpulinn@mail.ru>
2023-08-16 15:20:47 +0200

    Update NPCPointsMove.cs
    added vision rotation to move direction

--- Assets/Scripts/NPCPointsMove.cs
+++ Assets/Scripts/NPCPointsMove.cs
@@ -7,10 +7,13 @@ public class NPCPointsMove : MonoBehaviour
 {
     [SerializeField] private List<Transform> wayPoints;
     [SerializeField] private float waitAtPointTime = 5f;
+    // child component
+    [SerializeField] private Transform visionTransform;
 
     private NavMeshAgent _navMeshAgent;
     private int _currentWayPointIndex = 0;
     private bool _isMoving = true;
+    private Vector3 _lastRotation = Vector3.zero;
     
     void Start()
     {
@@ -18,7 +21,13 @@ public class NPCPointsMove : MonoBehaviour
 
         StartCoroutine(MoveNPC());
     }
-    
+
+    private void Update()
+    {
+        // rotate NPC's vision to movement direction
+        visionTransform.rotation = Quaternion.Euler(GetVisionRotation());
+    }
+
     private IEnumerator MoveNPC()
     {
         while (_isMoving)
@@ -38,5 +47,38 @@ public class NPCPointsMove : MonoBehaviour
             if (_currentWayPointIndex >= wayPoints.Count)
                 _currentWayPointIndex = 0;
         }
+    }
+
+    private Vector3 GetVisionRotation()
+    {
+        Vector3 rotation = _lastRotation;
+        if (Mathf.Abs(_navMeshAgent.desiredVelocity.x) > 1 && Mathf.Abs(_navMeshAgent.desiredVelocity.z) < 1)
+        {
+            rotation = new Vector3(0, _navMeshAgent.desiredVelocity.x > 0 ? 90 : -90, 0);
+        } else if (Mathf.Abs(_navMeshAgent.desiredVelocity.z) > 1 && Mathf.Abs(_navMeshAgent.desiredVelocity.x) < 1)
+        {
+            rotation = new Vector3(0, _navMeshAgent.desiredVelocity.z > 0 ? 0 : 180, 0);
+        } else if (Mathf.Abs(_navMeshAgent.desiredVelocity.x) >= 1 && Mathf.Abs(_navMeshAgent.desiredVelocity.z) >= 1)
+        {
+            if (_navMeshAgent.desiredVelocity.x > 0 && _navMeshAgent.desiredVelocity.z > 0)
+            {
+                rotation = new Vector3(0, 45, 0);
+            }
+            if (_navMeshAgent.desiredVelocity.x > 0 && _navMeshAgent.desiredVelocity.z < 0)
+            {
+                rotation = new Vector3(0, 135, 0);
+            }
+            if (_navMeshAgent.desiredVelocity.x < 0 && _navMeshAgent.desiredVelocity.z > 0)
+            {
+                rotation = new Vector3(0, -45, 0);
+            }
+            if (_navMeshAgent.desiredVelocity.x < 0 && _navMeshAgent.desiredVelocity.z < 0)
+            {
+                rotation = new Vector3(0, -135, 0);
+            }
+        }
+
+        _lastRotation = rotation;
+        return rotation;
     }
 }
