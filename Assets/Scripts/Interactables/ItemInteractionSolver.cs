9177198a3b421ca0dfcdfc8fe0524e36fe690454
Nikita <shpulinn@mail.ru>
2023-08-18 11:49:53 +0200

    Update ItemInteractionSolver.cs
    updated for item giving

--- Assets/Scripts/Interactables/ItemInteractionSolver.cs
+++ Assets/Scripts/Interactables/ItemInteractionSolver.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using Inventory;
 using UnityEngine;
 
 namespace Interactables
 {
     public class ItemInteractionSolver : MonoBehaviour, IPlayerInteractionSolver
     {
+        [SerializeField] private Item item;
         [SerializeField] private SpriteRenderer _renderer;
+
+        private InventoryManager _inventoryManager;
+
+        public bool _itemGaved = false;
+
+        private void Awake()
+        {
+            _inventoryManager = FindObjectOfType<InventoryManager>();
+        }
 
         public void Interact()
         {
@@ -20,6 +31,11 @@ namespace Interactables
         {
             // activate another item shader
             _renderer.material.SetColor("_Color", Color.red);
+            if (_itemGaved == false)
+            {
+                _inventoryManager.AddItem(item);
+                _itemGaved = true;
+            }
             return (PlayerAnimationsNames.Pick, false);
         }
     }
